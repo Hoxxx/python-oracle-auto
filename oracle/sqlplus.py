6b1e7610300789f3879242babdc7041bdb12a69c
Hoxxx <d3_neet@hotmail.com>
2018-10-24 15:25:20 +0800

    Create sqlplus.py

---
+++ oracle/sqlplus.py
@@ -0,0 +1,8 @@
+import os
+from subprocess import Popen, PIPE
+
+sqlplus = Popen(["sqlplus", "-S", "/", "as", "sysdba"], stdout=PIPE, stdin=PIPE)
+sqlplus.stdin.write("select sysdate from dual;"+os.linesep)
+sqlplus.stdin.write("select count(*) from all_objects;"+os.linesep)
+out, err = sqlplus.communicate()
+print out
